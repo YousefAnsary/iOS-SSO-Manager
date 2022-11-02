a147b54cd1c41c519a537dc8fad430021bef4e3b
Youssef El-Ansary <yousef.elansary@robustastudio.com>
2022-11-02 15:31:29 +0200

    refactor: remove comments

--- Package.swift
+++ Package.swift
@@ -17,10 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/google/GoogleSignIn-iOS", exact: "6.1.0"),
         .package(url: "https://github.com/google/GoogleSignIn-iOS", from: Version("6.1.0")),
         .package(url: "https://github.com/facebook/facebook-ios-sdk", from: Version("14.0.0"))
-//        .package(url: "https://github.com/facebook/facebook-ios-sdk", exact: "14.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
